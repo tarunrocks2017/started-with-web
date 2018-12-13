71ebd1785d0d6aac0218b629c88f773aaceb5f18
tarun kumar <tarunharsh2017@gmail.com>
2018-12-13 18:19:02 +0530

    code refactored

--- http-01.js
+++ http-01.js
@@ -1,27 +1,17 @@
-let file = require('fs');
-
-const http = require('http');
+const file1 = require("fs");
+const http = require("http");
 
 http.createServer((req,res) => {
-    if(req.url === "/showFile"){
-    file.readFile('index.html',function(err,pgResp){
-        if (err) {
-            res.writeHead(404);
-            res.write('Contents you are looking are Not Found');
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(pgResp);
-        }
-         
-        res.end();  
+    console.log(req.url);
+     file1.exists("/home/tarun/Documents/MountBlueProjects/04-http-project"+req.url,(bool) => {
+        file1.readFile("/home/tarun/Documents/MountBlueProjects/04-http-project"+req.url,(err,text) => {
+            if(err){
+            res.writeHead(404,"file not found ",{'content': 'text/html'});
+            }else{
+                res.write(text);
+            }
+        })
     });
-}else if(req.url === "/showImage"){
-    let imageFile = file.createReadStream('icon.png');
-       imageFile.pipe(res);
-       console.log(res);
-
-}else if(req.url === "/showBigFile"){
-    let bigFile = file.createReadStream("sample.txt");
-    bigFile.pipe(res);
-}
+    
 }).listen(8080);
+
